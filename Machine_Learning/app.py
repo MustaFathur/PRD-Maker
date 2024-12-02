568902a0493db9c6553baaf4b32d1973b4cc01ed
Fathur <fatur300904@gmail.com>
2024-12-02 21:00:49 +0700

    Update app.py

--- Machine_Learning/app.py
+++ Machine_Learning/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route('/api/generate-prd', methods=['POST'])
 def generate_prd():
