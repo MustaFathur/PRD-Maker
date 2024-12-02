83d5d130c31f3fd6a382b384bb4a558d5290b18f
Fathur <fatur300904@gmail.com>
2024-12-02 19:07:53 +0700

    Update Dockerfile

--- Cloud_Computing/backend/server.js
+++ Cloud_Computing/backend/server.js
@@ -43,7 +43,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/prd", prdRoutes);
 app.use("/api/personil", personilRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
