99404f437c0b6d95f7658bab5b27e2c917658a4b
fiziorh <fizio69@gmail.com>
2024-12-03 15:43:38 +0700

    [debug] dockerfile and main.py

--- Machine_Learning/app/main.py
+++ Machine_Learning/app/main.py
@@ -30,4 +30,4 @@ def generate_prd():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
