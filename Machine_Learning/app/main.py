c69a678e5e04e2ba168028004c5fd9cd70d93ffb
Fathur <fatur300904@gmail.com>
2024-12-04 18:06:53 +0700

    Update main.py

--- Machine_Learning/app/main.py
+++ Machine_Learning/app/main.py
@@ -1,9 +1,7 @@
 import sys
 import os
-import logging
 from flask import Flask, request, jsonify
 
-# Tambahkan path ke sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from llm import PRDGenerator
@@ -35,9 +33,5 @@ def generate_prd():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    openai_api_key = os.environ.get('OPENAI_API_KEY')
-    if not openai_api_key:
-        raise ValueError("No OPENAI_API_KEY set for Flask application")
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
