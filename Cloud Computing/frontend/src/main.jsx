1967dd72c52666edb142e39b4f1164eaf92915c6
upnormalmekah <106486766+upnormalmekah@users.noreply.github.com>
2024-12-05 00:07:39 +0700

    Update main.jsx

--- Cloud Computing/frontend/src/main.jsx
+++ Cloud Computing/frontend/src/main.jsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/output.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import LoginPage from "./login/Login";
+import DashboardPage from "./dashboard/DashboardPage";
+import Signup from "./components/signup/SignUp";
+import RegistrationPage from "./components/register/Register";
+import EditPRD from "./dashboard/prd/[prdId]/edit/EditPRD";
+import PRDPreview from "./dashboard/prd/components/preview/PRDPreview";
+// Import PRDPreview
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/dashboard/prd/:prdId/edit",
+        element: <EditPRD />,
+      },
+      {
+        path: "/dashboard/prd/preview",
+        element: <PRDPreview />, // Add PRDPreview route
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <RegistrationPage />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
+// Render the application
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
